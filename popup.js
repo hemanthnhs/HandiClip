2e3bc475b0abf65371e084183ee97c2e9eae9752
Hemanth Sai <hemanthsai10@gmail.com>
2021-05-13 00:23:19 -0400

    Copy and Delete from clipboard implemented

--- popup.js
+++ popup.js
@@ -20,19 +20,41 @@ window.addEventListener("load", () => {
   function renderClips() {
     clipsTable.innerHTML = "Hola";
     let newClips = "";
+    let btnIds = [];
+    let delBtnIds = [];
     for (const [key, value] of Object.entries(clips)) {
+      let id = key.replace(/\s/g, "-").toLowerCase();
       let newInnerHTML =
         "<div class='card'><div class='card-header row'><div class='col-8'>" +
         key +
         "</div>" +
-        "<div class='col-2'><button class='btn btn-outline-success btn-sm copy-btn float-end'>Copy</button></div>" +
-        "<div class='col-2'><button class='btn btn-outline-danger btn-sm copy-btn float-end'>Delete</button></div></div>" +
-        "<div class='card-body'> <p class='card-text'>" +
+        "<div class='col-2'><button id='" +
+        "btn-" +
+        id +
+        "' class='btn btn-outline-success btn-sm copy-btn float-end'>Copy</button></div>" +
+        "<div class='col-2'><button id='" +
+        "del-btn-" +
+        id +
+        "' class='btn btn-outline-danger btn-sm copy-btn float-end'>Delete</button></div></div>" +
+        "<textarea type='text' id='" +
+        "text-" +
+        id +
+        "' class='card-body form-control card-text' readonly>" +
         value +
-        "</p> </div> </div>";
+        "</textarea></div>";
+      btnIds.push("btn-" + id);
+      delBtnIds.push("del-btn-" + id);
       newClips = newClips + newInnerHTML;
     }
     clipsTable.innerHTML = newClips;
+    btnIds.forEach((btnId) => {
+      document.getElementById(btnId).addEventListener("click", copyToClipboard);
+    });
+    delBtnIds.forEach((btnId) => {
+      document
+        .getElementById(btnId)
+        .addEventListener("click", deleteFromClipboard);
+    });
   }
 
   function addClip(name, text) {
@@ -42,11 +64,40 @@ window.addEventListener("load", () => {
     });
   }
 
+  function copyToClipboard(ev) {
+    let btnId = ev.target.id;
+    let id = btnId.split("btn-")[1];
+    let textId = "text-" + id;
+    document.getElementById(textId).select();
+    document.execCommand("copy");
+  }
+
+  function deleteFromClipboard(ev) {
+    let btnId = ev.target.id;
+    let id = btnId.split("del-btn-")[1];
+    delete clips[id];
+    chrome.storage.sync.set({ clips }, function () {
+      renderClips();
+    });
+  }
+
+  document.getElementById("add-new-btn").addEventListener("click", function () {
+    addSection.style.display = "block";
+    clipSection.style.display = "none";
+  });
+
   document.getElementById("submit-btn").addEventListener("click", function () {
     let cNameDom = document.getElementById("cname");
     let cTextDom = document.getElementById("ctext");
     let name = cNameDom.value;
     let text = cTextDom.value;
     addClip(name, text);
+    addSection.style.display = "none";
+    clipSection.style.display = "block";
+  });
+
+  document.getElementById("cancel-btn").addEventListener("click", function () {
+    addSection.style.display = "none";
+    clipSection.style.display = "block";
   });
 });
